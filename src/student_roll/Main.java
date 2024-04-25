c74300832e9d58a971f2b26027f5c5cb309f1d86
Steven Mancine <steven.mancine@charter.com>
2024-04-24 22:28:53 -0600

    Add text area

--- src/student_roll/Main.java
+++ src/student_roll/Main.java
@@ -1,21 +1,12 @@
 package student_roll;
 
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.util.Date;
-import java.util.Random;
-
-import javax.swing.Action;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.input.MouseEvent;
 import javafx.event.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -29,15 +20,17 @@ public class Main extends Application  {
   Pane pane;
   GridPane gridPane;
   BorderPane borderPane;
-  Label studentNameLabel, studentAddressLabel, studentGPALabel;
-  TextField studentNameText, studentAddressText, studentGPAText;
+  Label nameLabel, addressLabel, GPALabel, studentsListLabel;
+  TextField nameText, addressText, GPAText;
   Scene scene;
+  StudentList students;
+  TextArea studentsList;
 
   @Override
   public void start(Stage primaryStage) throws Exception {
+    students = new StudentList();
     borderPane = new BorderPane();
     gridPane = new GridPane();
-    gridPane.setGridLinesVisible(true);
     gridPane.setAlignment(Pos.TOP_CENTER); 
     gridPane.setPadding(new Insets(20));
     gridPane.setStyle("-fx-border-color: black;");
@@ -48,39 +41,55 @@ public class Main extends Application  {
     save = new MenuItem("Save");
     menuButton = new MenuButton("Menu", null, save);
     hbox = new HBox(menuButton);
-    hbox.setPadding(new Insets(15, 12, 15, 12));
+    hbox.setPadding(new Insets(10, 12, 10, 12));
     hbox.setSpacing(10);
     hbox.setStyle("-fx-background-color: #336699;");
     borderPane.setTop(hbox);
 
     // labels
-    studentNameLabel = new Label("Student Name: ");
-    studentNameText = new TextField("");
-    gridPane.add(studentNameLabel, 0, 0);
-    gridPane.add(studentNameText, 1, 0);
-    studentAddressLabel = new Label("Student address: ");
-    studentAddressText = new TextField("");
-    gridPane.add(studentAddressLabel, 0, 1);
-    gridPane.add(studentAddressText, 1, 1);
-    studentGPALabel = new Label("Student GPA: ");
-    studentGPAText = new TextField("");
-    gridPane.add(studentGPALabel, 0, 2);
-    gridPane.add(studentGPAText, 1, 2);
+    nameLabel = new Label("Student Name: ");
+    nameText = new TextField("");
+    gridPane.add(nameLabel, 0, 0);
+    gridPane.add(nameText, 1, 0);
+    addressLabel = new Label("Student address: ");
+    addressText = new TextField("");
+    gridPane.add(addressLabel, 0, 1);
+    gridPane.add(addressText, 1, 1);
+    GPALabel = new Label("Student GPA: ");
+    GPAText = new TextField("");
+    gridPane.add(GPALabel, 0, 2);
+    gridPane.add(GPAText, 1, 2);
     
     // Button 
     submitButton = new Button("Add Student");
     gridPane.add(submitButton, 0, 3, 2, 1);
     GridPane.setHalignment(submitButton, HPos.CENTER);
 
+    // List
+    studentsListLabel = new Label("Students");
+    gridPane.add(studentsListLabel, 0, 4);
+    studentsList = new TextArea();
+    studentsList.setEditable(false);
+    gridPane.add(studentsList, 0, 5, 2, 1);
+
     submitButton.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent event) {
-        primaryStage.close();
+        try {
+          students.add(nameText.getText(), addressText.getText(), Double.parseDouble(GPAText.getText()));
+          nameText.setText("");
+          addressText.setText("");
+          GPAText.setText("");
+          studentsList.setText(students.toString());
+        } catch (Exception e) {
+          Alert alert = new Alert(AlertType.ERROR, e.getMessage());
+          alert.showAndWait();
+        }
       }
     });
     
     borderPane.setCenter(gridPane);
-    scene = new Scene(borderPane, 500, 300);
+    scene = new Scene(borderPane, 500, 400);
 
     primaryStage.setTitle("User Interface");
     primaryStage.setScene(scene);
